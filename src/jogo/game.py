57fd10fbc60b20eaa46a93ebb531cb006ab3c1a1
João Pedro Spósito Pereira Cézar <joaopedrosposito6@gmail.com>
2026-06-03 17:21:07 -0300

    feat: hitbox e movimentação

---
+++ src/jogo/game.py
@@ -0,0 +1,28 @@
+import pygame
+import sys
+
+pygame.init()
+
+class carro:
+    def __init__(self, x, y):
+        self.rect = pygame.rect(x, y, 40, 70)
+        self.cor = (0,128, 255)
+        self.velocidade = 6
+    def mover(self, teclas, limite_pista):
+        limite_esq, limite_dir, limite_cima, limite_baixo = limite_pista
+
+        if teclas[pygame.K_LEFT] and self.rect.left > limite_esq:
+            self.rect.x -= self.velocidade
+        if teclas[pygame.K_RIGHT] and self.rect.right < limite_dir:
+            self.rect.x += self.velocidade
+        if teclas[pygame.K_UP] and self.rect.top > limite_cima:
+            self.rect.y -= self.velocidade
+        if teclas[pygame.K_DOWN] and self.rect.bottom < limite_baixo:
+            self.rect.y += self.velocidade
+    
+    def checar_colisao(self, hitboxes_obstaculos):
+        if self.rect.collidelist(hitboxes_obstaculos) != -1:
+            return True
+        return False
+    def range(self,tela):
+        pygame.draw.rect(tela, self.cor, self.rect)
